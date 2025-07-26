d83dfd48c13cecc6aed5ade4a058e22a569ea72a
XtianFreolo <F60@umbc.edu>
2025-07-25 20:47:52 -0400

    Worked on Content and Home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+function HomePage() {
+    return (
+        <>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main content area.</p>
+            <p>Explore the champions and their unique abilities!</p>
+            <p>Use the navigation bar to access different content pages.</p>
+        </>
+    );
+}
+
+export default HomePage;
